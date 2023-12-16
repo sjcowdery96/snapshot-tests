45e5385bc2c19bbf3b261090485dbf53954812b5
sjcowdery96 <sjc.cowdery@gmail.com>
2023-12-15 21:06:18 -0600

    last few changes and comments

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,17 @@ import renderer from 'react-test-renderer'
 
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders my Github Bio', async () => {
+  //can't get this test to work even when my actual bio renders...
+  //fetch.mockResponseOnce(JSON.stringify({ myBio: "Let's get technical." }))
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const myBio = await waitFor(() => screen.getByRole('button', screen.getByRole('heading', { level: 2 })))
+  expect(myBio).toHaveTextContent("Let's get technical.");
 });
 
 test('renders a snapshot', () => {
   const tree = renderer.create(<App />).toJSON()
   expect(tree).toMatchSnapshot()
 })
+
+
